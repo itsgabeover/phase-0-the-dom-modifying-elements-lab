58e8d43c8588bc6f567a704b1a0b39d821111b46
itsgabeover <gabe.enrique.miranda@gmail.com>
2021-06-19 13:28:53 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById('main');
+document.body.removeChild(main);
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion"
+document.body.appendChild(newHeader);
